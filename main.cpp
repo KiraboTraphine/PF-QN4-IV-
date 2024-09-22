e1aa735b36fc47abf79aed90f863d5d49620f9a7
KiraboTraphine <kirabodaphinetraphine@gmail.com>
2024-09-22 12:24:31 +0300

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    string email;
+
+
+    cout << "Enter your email address: ";
+    cin >> email;  // Get input from the user
+
+
+    cout << "You entered: " << email << endl;
+
+    return 0;
+}
+
